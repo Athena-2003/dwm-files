ba5331d4394b162acae2cfd3c1c998c9dc4f3d73
Athena-2003 <bnshashankr1597@gmail.com>
2024-10-24 20:43:03 +0530

    added dwmblocks for bar

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"Bat:", "acpi | awk '/^Bat/ {print $4}'| cut -c-3",	10,		0},
+	{"Vol:", "amixer sget Master | awk '/Left:/ { print $5 }' | cut --characters 2-4",	10,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
